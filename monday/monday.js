2b00e77b06959781c845a16e11be29f106e4208c
Rauf GUVEN <mrguven@gmail.com>
2022-10-18 17:55:35 +0200

    new

--- monday/monday.js
+++ monday/monday.js
@@ -1,4 +1,5 @@
-/*1
+
+console.log('//1'); /// there are head of problem
 
 
 for (var i = 0; i < 136; i++) {
@@ -6,25 +7,22 @@ for (var i = 0; i < 136; i++) {
 }
 
 
-//2
+
+console.log('//2'); //head
 
 for (var i = 1; i < 136; i+=2) {
     console.log(i)
 }
 
-*/
+
+console.log('//3'); // head
 
 
-console.log('//3');
-
-
-let sum=0;
-
+let sum = 0;
 for (var i = 0; i < 136; i++) {
-    sum  = sum +  i;
+    sum  = sum +  i ;
+    console.log('Number is: ' + i + ' Sum: ' +  sum );
 }
-
-console.log('Number is: ' + i + ' Sum: ' +  sum + "</br>");
 
 
 
@@ -33,17 +31,10 @@ console.log('//4');
 
 
 const xar = [1,4,2,12];
-
-
-    console.log(xar);
-
-
-
-
+for (let m = 0; m < xar.length; m++)
+console.log(xar[m]);
 console.log('//5');
-
 const xy = [2,-3,-1];
-
 console.log(Math.max(...xy));  //what means three point and is there ant other methods?
 
 
@@ -52,21 +43,42 @@ console.log('//6');
 
 const xyz = [2,1,3];
 
-
-
 const c = 0;
-
 const average = xyz.reduce((a,b) => a + b,c);
-
 console.log(average / xyz.length)
+
+//another way to solve the problem
+let = mrs = 0;
+for(let asd = 0; asd <xyz.length; asd++){
+    mrs = xyz[asd] + mrs
+}
+console.log(mrs / xyz.length);
+
+
 
 
 console.log('//7');
 
+
 const negative = [2,-1,4,-3];
-
-
+for (let i = 0; i < negative.length;i++){
+    if(negative[i] < 0) {
+        negative[i] = 0;
+    }
+}
+console.log(negative);
 
 
 
 console.log('///8')
+
+
+
+let numberString = [1,-4,0,-1];
+for (let n = 0; n < numberString.length; n++){
+    if(numberString[n] < 0 ) {
+        numberString[n] = 'Turing';
+    }
+}
+
+console.log(numberString);
