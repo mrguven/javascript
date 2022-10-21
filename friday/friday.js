733da5ef005bbf93b172901be2bebeba7dbfd5af
Rauf GUVEN <mrguven@gmail.com>
2022-10-21 13:38:36 +0200

    update

--- friday/friday.js
+++ friday/friday.js
@@ -1,13 +1,19 @@
 
-/*
+
+
+// I did not solve this problem.
+
 let arr = [5, 2, 8, 20, -2, 0, 11, 7, 3, 9, 31];
+  let i=0;
 
-
-
-
-    for(let i = 0; i <arr.length < i++) {
-        if (arr[i] > arr)
+while (i<arr.length){
+    if (arr[i]<arr[i+1]){
+        temp = arr[i]
+        arr[i] = arr[i+1]
+        arr[i+1]=temp
+        i=-1
     }
+    i++;
+}
 
-
-*/
+console.log(arr[2])
