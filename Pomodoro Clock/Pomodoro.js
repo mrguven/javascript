bf9d4091a42861eaf392d4dc49970aa1829d1959
Rauf GUVEN <mrguven@gmail.com>
2022-11-03 12:35:14 +0100

    update

---
+++ Pomodoro Clock/Pomodoro.js
@@ -0,0 +1,25 @@
+
+
+
+/*const setWork = document.getElementById("incTime").addEventListener("click", incNumber);
+
+function incNumber () {
+       let nbr = 0;
+       nbr++;
+        
+}
+
+
+
+document.getElementById("breakTime").innerHTML = setWork;
+
+*/
+
+function myFunction (a,b) {
+       if (b === 0) {
+              return 1;
+              }
+       else {
+              return a * 
+       }
+}
