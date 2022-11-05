df251fadf1f2ab97ea6180052ee080d2a9b64db9
Rauf GUVEN <mrguven@gmail.com>
2022-11-05 22:18:14 +0100

    update

--- Pomodoro Clock/Pomodoro.js
+++ Pomodoro Clock/Pomodoro.js
@@ -6,18 +6,116 @@
 
 
 
- document.getElementById("incTime").addEventListener("click", incNumber());
- var userInPut1;
- function incNumber () {
-      
-        
+ document.getElementById("incTime").addEventListener("click", setText);
+ 
+ 
+     
+
+
+function setText () {
+       document.getElementById("bSec").innerHTML = incNumber();
+       
+       }
+       function incNumber () {
+       
+
+              return  parseFloat( document.getElementById("bSec").innerHTML) +1 ;
+       }
+
+
+document.getElementById("minTime").addEventListener("click", setSec);
+ 
+ function setSec () {
+       
+document.getElementById("bSec").innerHTML = minNumber();
+     
+     
 }
 
-document.getElementById("breakTime").innerHTML=userInPut1;
+function minNumber () {
+       
+       return  parseFloat( document.getElementById("bSec").innerHTML) -1 ;
 
-userInPut1 = 
+     
+}
 
 
 
-
+document.getElementById("incTime-1").addEventListener("click", setWorkMin);
  
+ function setWorkMin () {
+       document.getElementById("wSec").innerHTML = incNumber();
+       
+       }
+       function incNumber () {
+       
+
+              return  parseFloat( document.getElementById("wSec").innerHTML) +1 ;
+       }
+
+
+
+
+document.getElementById("minTime-1").addEventListener("click", setWork);
+ 
+ function setWork () {
+       
+document.getElementById("wSec").innerHTML = minWorkSec();
+     
+     
+}
+
+function minWorkSec () {
+       
+       return  parseFloat( document.getElementById("wSec").innerHTML) -1 ;
+       
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+document.getElementById("startButton").addEventListener("click", startTime);
+
+var min=24;
+var sec=59;
+function startTime () {
+      
+       function updateTime () {
+       if (sec>0) {
+              sec--;
+       
+       }
+       else  if (min>0){
+                     min--;
+                     sec=59;
+              }
+       
+
+              document.getElementById("demo").innerHTML = min + ":" +sec;
+};
+
+updateTime();
+
+setInterval(updateTime,1000);
+
+
+}
+
+document.getElementById("pauseButton").addEventListener("click", pauseTime);
+
+function pauseTime (){
+       clearInterval( updateTime);
+}
